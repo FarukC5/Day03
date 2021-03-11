4102cdf4c582621016d09980d8e11e3298f2be65
FarukC5 <faruk_cajic@yahoo.com>
2021-03-11 08:41:27 +0100

    added

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+
+    // nefunkcionise
+    
+    /* nema funkcije trenutno i treba
+    pokusati ispraviti gresku */
+
+}
+
+console.log(main());
+
+module.exports = main;
---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+
+    var x = 10;
+    
+    var y = 10;
+
+    var x = y;
+   
+    return [x, y];
+}
+
+console.log(main());
+
+module.exports = main;
